0bf0cccfbc49aff05d9427b3d80faee9b17239aa
ishan jadhav <ishanjadhav6126@gmail.com>
2026-07-12 18:17:18 +0530

    chore: revert to default Cloudflare setup

--- Main website/vite.config.ts
+++ Main website/vite.config.ts
@@ -12,7 +12,4 @@ export default defineConfig({
     // nitro/vite builds from this
     server: { entry: "server" },
   },
-  nitro: {
-    preset: process.env.VERCEL ? "vercel-edge" : "cloudflare-module",
-  },
 });
